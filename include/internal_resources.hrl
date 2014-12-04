d6ee60557fb36ab6ead579a2fdc4932110bcb1a0
Andrey Nikiforov <a.d.nikiforov@gmail.com>
2014-12-04 13:53:17 +0300

    Add some internal resources

---
+++ include/internal_resources.hrl
@@ -0,0 +1,12 @@
+%%%-------------------------------------------------------------------
+%%% @author adnikiforov@gmail.com
+%%% @copyright (C) 2014
+%%% @doc
+%%% Internal resources
+%%% @end
+%%% Created : 28. Nov 2014 0:40
+%%%-------------------------------------------------------------------
+-author("a.d.nikiforov@gmail.com").
+
+%% Default user-agent
+-define(DEFAULT_UA, {"User-Agent", "UA"}).
